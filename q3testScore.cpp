3c8b4e1328a5e494bcd904c95dd2a3b09cfe63c3
PJiggy <pjinocencio@gmail.com>
2025-10-07 12:57:45 -0700

    completed the function to enter test scores. All that's left is the calculations function.

--- q3testScore.cpp
+++ q3testScore.cpp
@@ -3,5 +3,39 @@
 
 using namespace std;
 
-// create the vector for 
+// create the vector for student scores 
+vector<double> scores;
 
+void testsAmmount(){
+    int tests; // temp variable for tests
+    
+    // prompt user to enter amount of tests to be graded
+    cout << "Enter the amount of tests graded: ";  
+    cin >> tests; 
+    while(tests < 0){ // if users decides to be dumb
+        cout << "How would you grade negative tests?...\nBe serious:"; 
+        cin >> tests;
+    }
+    // now to resize the vector for the amount of tests
+    scores.resize(tests);
+}
+
+void testEnter(){
+    // prompt for user to enter test scores
+    cout << "Please enter " << scores.size() << " scores:";
+
+    int value;// temp for value to be read in
+    // need to enter all values
+    for(int i = 0; i < scores.size(); i++){
+        // output a space for formatting between values
+        cout << " ";
+        cin >> value;
+        while(value < 0){ // only if user enters negative
+            cout << "\nTests cannot be negative scores. Continue:";
+            cin >> value; 
+        }
+        scores[i] = value; // store value at location
+    }
+}
+
+// need to do the calculations function
